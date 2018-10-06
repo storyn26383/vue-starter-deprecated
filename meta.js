e879c3753c6ca62900c7face5920c406ee8a1c38
Sasaya <storyn26383@gmail.com>
2018-10-06 14:43:15 +0800

    support pug, scss and lint all code

--- meta.js
+++ meta.js
@@ -23,4 +23,3 @@ module.exports = {
   },
   completeMessage: '{{#inPlace}}To get started:\n\n  npm install # Or yarn\n  npm run dev{{else}}To get started:\n\n  cd {{destDirName}}\n  npm install # Or yarn\n  npm run dev{{/inPlace}}'
 };
-
--- template/nuxt.config.js
+++ template/nuxt.config.js
@@ -1,3 +1,5 @@
+const StyleLintPlugin = require('stylelint-webpack-plugin')
+
 module.exports = {
   /*
   ** Headers of the page
@@ -7,7 +9,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '{{escape description }}' }
+      { hid: 'description', name: 'description', content: '{{ escape description }}' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -16,24 +18,35 @@ module.exports = {
   /*
   ** Customize the progress bar color
   */
-  loading: { color: '#3B8070' },
+  loading: { color: '#3b8070' },
   /*
   ** Build configuration
   */
   build: {
-    /*
-    ** Run ESLint on save
-    */
     extend (config, { isDev, isClient }) {
+      /*
+      ** Run ESLint on save
+      */
       if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.vue$/,
+          loader: 'vue-pug-lint-loader',
+          options: require('./.puglintrc.js'),
+          exclude: /node_modules/
+        })
+
         config.module.rules.push({
           enforce: 'pre',
           test: /\.(js|vue)$/,
           loader: 'eslint-loader',
-          exclude: /(node_modules)/
+          exclude: /node_modules/
         })
+
+        config.plugins.push(new StyleLintPlugin({
+          files: ['**/*.{vue,htm,html,css,sss,less,scss,sass}']
+        }))
       }
     }
   }
 }
-
